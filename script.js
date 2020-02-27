9629d2bd099fa0cbe55d4967a6d3af9ec0f0f110
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-27 02:24:14 +0000

    Changing scores

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var location;
+	var location, index;
 	// show results
 	$("#submit").click(function(){
 		location = document.getElementById("location").value;
@@ -15,42 +15,12 @@ $(document).ready(function(){
 			success: function(data){
 				console.log(data.list);
 				// document.getElementById("output").innerHTML = "";
-				var best = -1000;
-				for(var i = 0; i < data.list.length; i++){
-					var score = 0;
-					if ((data.list[i].main.feels_like >= 16 ||
-					    data.list[i].main.feels_like <= 19) || 
-						data.list[i].weather[0].main == "Clear" ||
-						data.list[i].wind.speed < 4)score++;
-					
-					if (data.list[i].main.feels_like > 19 && 
-						data.list[i].weather[0].main == "Clear" &&
-						data.list[i].wind.speed < 2)score += 4;
-					
-					if (data.list[i].main.feels_like > 19 && 
-						data.list[i].weather[0].main == "Clear" &&
-						data.list[i].wind.speed < 4 &&
-						data.list[i].wind.speed >= 2)score += 2;
-					
-					if (data.list[i].main.feels_like < 16 || 
-						data.list[i].weather[0].main == "Rain" ||
-						(data.list[i].wind.speed >= 4 || 
-						data.list[i].wind.speed <= 8))score--;
-					
-					if (data.list[i].main.feels_like < 10 || 
-						data.list[i].weather[0].main == "Extreme" ||
-						data.list[i].wind.speed > 8)score -= 5;
-					if(score > best){
-						best = score;
-						var index = i;
-					}
-					console.log(best, index, score, toDateTime(data.list[i].dt));
-				}
+				successAjax(data);
 			},
 			error: function(){console.log("error");}
 		}); //end of ajax
 	}
-});
+
 
 function toDateTime(secs) {
 	secs = secs + 46800;
@@ -58,3 +28,69 @@ function toDateTime(secs) {
     t.setSeconds(secs);
     return t;
 }
+
+function successAjax(data){
+	var best = -1000;
+	console.log(location);
+	var topArray = [];
+	for(var i = 0; i < data.list.length; i++){
+		var score = 0;
+		// make better scores
+		if ((data.list[i].main.feels_like >= 16 ||
+			data.list[i].main.feels_like <= 19) || 
+			data.list[i].weather[0].main == "Clear" ||
+			data.list[i].wind.speed < 4)score++;
+
+		if (data.list[i].main.feels_like > 19 && 
+			data.list[i].weather[0].main == "Clear" &&
+			data.list[i].wind.speed < 2)score += 4;
+
+		if (data.list[i].main.feels_like > 19 && 
+			data.list[i].weather[0].main == "Clear" &&
+			data.list[i].wind.speed < 4 &&
+			data.list[i].wind.speed >= 2)score += 3;
+
+		if (data.list[i].main.feels_like > 19 || 
+			data.list[i].wind.speed < 2)score += 2;
+			
+		if (data.list[i].main.feels_like < 16 || 
+			data.list[i].weather[0].main == "Clouds" ||
+			(data.list[i].wind.speed >= 4 || 
+			data.list[i].wind.speed <= 8))score--;
+
+		if (data.list[i].main.feels_like < 16 || 
+			data.list[i].weather[0].main == "Rain" ||
+			(data.list[i].wind.speed >= 4 || 
+			data.list[i].wind.speed <= 8))score -= 2;
+
+		if (data.list[i].main.feels_like < 10 || 
+			data.list[i].weather[0].main == "Extreme" ||
+			data.list[i].wind.speed > 8)score -= 5;
+
+		if (score > best){
+			best = score;
+			index = i;
+			topArray = [];
+			// var time = toDateTime(data.list[i].dt).getHours();
+			var obj = data.list[i];
+			topArray.push(obj);
+		}
+
+		else if (score == best){
+			var obj = data.list[i];
+			topArray.push(obj);
+		}
+
+		console.log(best, index, score, toDateTime(data.list[i].dt));
+	}
+	console.log(topArray);
+	compareDates(topArray);
+}
+
+function compareDates(arr){
+	for(var i = 0; i<arr.length; i++){
+		// console.log(toDateTime(arr[i].dt).getHours())
+	}
+}
+
+});
